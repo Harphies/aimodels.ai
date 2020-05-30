a683e0c38fd0ee6776916d265059efd8f8f0b932
Harphies <tafeez.tech@gmail.com>
2020-05-30 10:57:18 +0100

    'aimodels'

---
+++ app.py
@@ -0,0 +1,70 @@
+# Exposing the model via an API
+import joblib
+from flask import Flask, request
+import numpy as np
+import pandas as pd
+from flasgger import Swagger
+
+
+# Load the model
+model = joblib.load('models/clf.pkl')
+
+
+# Initiate the Flask App
+app = Flask(__name__)
+swagger = Swagger(app)
+
+
+@app.route('/predict')
+def predicti_iris():
+    # Grab all the input features that is needed for the prediction
+    """Example endpoint returning a prediction of iris
+    ---
+    parameters:
+      - name: s_length
+        in: query
+        type: number
+        required: true
+      - name: s_width
+        in: query
+        type: number
+        required: true
+      - name: p_length
+        in: query
+        type: number
+        required: true
+      - name: p_width
+        in: query
+        type: number
+        required: true
+    """
+    s_length = request.args.get("s_length")
+    s_width = request.args.get("s_width")
+    p_length = request.args.get("p_length")
+    p_width = request.args.get("p_width")
+
+    # Convrt to numpy array and ensure the other of the input features
+    prediction = model.predict(
+        np.array([[s_length, s_width, p_length, p_width]]))
+
+    return str(prediction)
+
+
+@app.route('/predict_file', methods=['POST'])
+def predict_file():
+    """Example file endpoint returning a prediction of iris
+    ---
+    parameters:
+      - name: input_file
+        in: formData
+        type: file
+        required: true
+    """
+    input_data = pd.read_csv(request.files.get('input_file'), header=None)
+    prediction = model.predict(input_data)
+    return str(list(prediction))
+
+
+if __name__ == '__main__':
+    # app.run(host='0.0.0.0', port=3000)
+    app.run(debug=True)
